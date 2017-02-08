2ced91d8ecaedb50539d9489564f855e18a7db4d
Matthew Shelto <mrhenryford59@gmail.com>
2017-02-08 00:26:38 +0000

    Update joyStick.java

--- src/org/usfirst/frc/team181/robot/joyStick.java
+++ src/org/usfirst/frc/team181/robot/joyStick.java
@@ -1,43 +1,52 @@
 package org.usfirst.frc.team181.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.*;
 
 public class joyStick {
 
 	//create joystick
 	public static Joystick joystick = new Joystick(0);
+	public static Joystick opStick = new Joystick(1);
 	static boolean highGear = false;
+	static boolean collector_open = false;
 	
 	//create joystick buttons
 	JoystickButton gearButton = new JoystickButton(joystick, 1);
 	
 	public static void doButtons(){
-		//if button one on the joystick is pressed, and high gear is not already on
 		if(joystick.getRawButton(1) == true && highGear == false){
-			//tell the driver high gear is turning on
-			System.out.println("Engageing High Gear!");
-			//turn on high gear
+			System.out.println("Engaging High Gear!");
 			DriveTrain.highGear();
 			highGear = true;
 		}
-		//if button one is released and high gear is already on
 		if(joystick.getRawButton(1) == false && highGear == true){
-			//tell the driver low gear is turning on
 			System.out.println("Going to Low Gear!");
-			//turn on low gear
 			DriveTrain.lowGear();
 			highGear = false;
 		}
+		if (joystick.getRawButton(3) == true) {
+			DriveTrain.resetEncoders();
+		}
+		if(opStick.getRawButton(2) == true && collector_open == false){
+			Mechanisms.collectorOpen();
+			collector_open = true;
+		}
+		if(opStick.getRawButton(2) == false && collector_open == true){
+			Mechanisms.collectorClosed();
+			collector_open = false;
+		}	 		 			
+		
 	}
 	
-	//return a decimal with the current position of the y axis on the joystick
+	
+
+
 	public static double getY(){
 		return joystick.getY();
 	}
 	
-	//return a decimal with the current position of the z azis on the joystick
-	public static double getZ(){
+	public static double getZ(){	
 		return joystick.getZ();
 	}
 	
