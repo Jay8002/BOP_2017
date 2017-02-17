c1a7985dda8f94a96465b4351c552e80980aceb2
Matthew Shelto <mrhenryford59@gmail.com>
2017-02-16 19:29:34 -0500

    fixing

--- src/org/usfirst/frc/team181/robot/Mechanisms.java
+++ src/org/usfirst/frc/team181/robot/Mechanisms.java
@@ -2,13 +2,22 @@ package org.usfirst.frc.team181.robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.Servo;
+
 
 public class Mechanisms{
 	static VictorSP shooterMotor = new VictorSP(2);
 	//create DoubleSolenoids for gear collector
 	static DoubleSolenoid collectorSolenoid = new DoubleSolenoid(0,2,3);
 	static boolean collector_closed = true;
+	static Servo climberServo = new Servo(3);
 	
+	public static void servoOpen(){
+		climberServo.setAngle(22);
+	}
+	public ststic void servoClosed(){
+		climberServo.setAngle(50);
+	}
 	public static void collectorOpen (){
 		collectorSolenoid.set(DoubleSolenoid.Value.kForward);
 	}
